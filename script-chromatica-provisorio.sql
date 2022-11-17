c2660ef38a284f885eeeb66782a20ff2d12f9f9b
EnzoMedej <enzo.guimaraes@sptech.school>
2022-11-17 00:46:29 -0300

    atualizacoes na database

--- script-chromatica-provisorio.sql
+++ script-chromatica-provisorio.sql
@@ -1,34 +1,43 @@
 create database chromatica;
 use chromatica;
 
-create table usuario(
-idUsuario int primary key auto_increment,
-nome varchar(45),
-sobrenome varchar(45),
-senha varchar(45),
- fkTribos int,
- foreign key (fkTribos) references tribo(idTribos)
-);
-
 create table tribo(
-idTribos int primary key auto_increment,
-nome varchar(45),
-categoria varchar(45)
+idTribo int primary key auto_increment,
+nome varchar(45)
+);
+ 
+create table questionario(
+idQuestionario int primary key auto_increment,
+pergunta_1 varchar(45),
+pergunta_2 varchar(45),
+pergunta_3 varchar(45),
+pergunta_4 varchar(45),
+pergunta_5 varchar(45),
+pergunta_6 varchar(45)
 );
 
-create table questionario(
-idQuestionario int auto_increment,
-resposta1 varchar(45),
-resposta2 varchar(45),
-resposta3 varchar(45),
-resposta4 varchar(45),
-resposta5 varchar(45),
-resposta6 varchar(45),
+CREATE TABLE usuario (
+	idUsuario INT primary key auto_increment,
+	nome VARCHAR(45),
+	sobrenome VARCHAR(45),
+	email VARCHAR(45),
+	senha VARCHAR(45),
+    fktribos int,
+    foreign key(fkTribos) references tribo(idTribo),
+    fkQuestionario int,
+    foreign key(fkQuestionario) references questionario(idQuestionario)
+); 
+
+create table resposta(
 fkTribos int,
-foreign key(fktribos) references tribo(idTribos),
-fkUsuario int,
-foreign key(fkUsuario) references usuario(idUsuario),
- primary key (idQuestionario, fktribos, fkUsuario)
+foreign key(fkTribos) references tribo(idTribo),
+fkResposta int,
+foreign key(fkResposta) references questionario(idQuestionario)
 );
+
+
 
 select * from usuario;
+select * from tribo;
+select * from questionario;
+select * from resposta;
