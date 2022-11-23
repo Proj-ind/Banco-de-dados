59851be26898d32e82312d8d6ddee8567fe47c4e
EnzoMedej <enzo.guimaraes@sptech.school>
2022-11-22 21:48:48 -0300

    mudanças dentro do bd

--- script-chromatica-provisorio.sql
+++ script-chromatica-provisorio.sql
@@ -5,39 +5,35 @@ create table tribo(
 idTribo int primary key auto_increment,
 nome varchar(45)
 );
- 
-create table questionario(
-idQuestionario int primary key auto_increment,
-pergunta_1 varchar(45),
-pergunta_2 varchar(45),
-pergunta_3 varchar(45),
-pergunta_4 varchar(45),
-pergunta_5 varchar(45),
-pergunta_6 varchar(45)
-);
 
 CREATE TABLE usuario (
 	idUsuario INT primary key auto_increment,
 	nome VARCHAR(45),
 	sobrenome VARCHAR(45),
 	email VARCHAR(45),
-	senha VARCHAR(45),
-    fktribos int,
-    foreign key(fkTribos) references tribo(idTribo),
-    fkQuestionario int,
-    foreign key(fkQuestionario) references questionario(idQuestionario)
-); 
-
-create table resposta(
-fkTribos int,
-foreign key(fkTribos) references tribo(idTribo),
-fkResposta int,
-foreign key(fkResposta) references questionario(idQuestionario)
+	senha VARCHAR(45)
 );
 
+create table resposta(
+idresposta int,
+fkusuario int,
+foreign key(fkusuario) references usuario(idUsuario),
+fkTribo int,
+foreign key(fkTribo) references tribo(idTribo),
+primary key (idresposta, fkusuario, fktribo)
+);
 
 
 select * from usuario;
 select * from tribo;
-select * from questionario;
 select * from resposta;
+
+
+insert into tribo values
+(null, 'kidness punks'),
+(null, 'eco warriors'),
+(null, 'junkyard scavengers'),
+(null, 'cyber kids'),
+(null, 'freedom fighters'),
+(null, 'government officials');
+
