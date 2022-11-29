ac73e3aa319546965de9a1be834ff4d22f5c414d
EnzoMedej <enzo.guimaraes@sptech.school>
2022-11-29 11:39:28 -0300

    envio para a facul

--- script-chromatica-provisorio.sql
+++ script-chromatica-provisorio.sql
@@ -11,11 +11,13 @@ CREATE TABLE usuario (
 	nome VARCHAR(45),
 	sobrenome VARCHAR(45),
 	email VARCHAR(45),
-	senha VARCHAR(45)
+	senha VARCHAR(100),
+    fkTriboUser int,
+    foreign key(fkTriboUser) references tribo(idTribo)
 );
 
 create table resposta(
-idresposta int,
+idresposta int auto_increment,
 fkusuario int,
 foreign key(fkusuario) references usuario(idUsuario),
 fkTribo int,
@@ -23,12 +25,10 @@ foreign key(fkTribo) references tribo(idTribo),
 primary key (idresposta, fkusuario, fktribo)
 );
 
-
 select * from usuario;
 select * from tribo;
 select * from resposta;
-
-
+ 
 insert into tribo values
 (null, 'kidness punks'),
 (null, 'eco warriors'),
@@ -37,3 +37,12 @@ insert into tribo values
 (null, 'freedom fighters'),
 (null, 'government officials');
 
+insert into usuario values
+(null, 'Enzo', 'Medej', 'enzomedej@gmail.com', 'silvio123', 1), 
+(null, 'Alice', 'Guimaraes', 'alicegui@gmail.com', '11viviane', 2), 
+(null, 'silvio', 'tavares', 'silviotav@gmail.com', 'julysy11', 2);
+
+ update usuario set fkTriboUser = 3 where idUsuario = 3; 
+
+select t.nome, count(u.idusuario) as qtd from tribo t
+join usuario u on fkTriboUser = idtribo group by t.nome;
